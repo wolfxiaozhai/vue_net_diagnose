620aaef7fcf4c782f902703dfb61ad02ff06d2ac
zhaoyoushuai <zhaoyoushuai@meituan.com>
2017-01-13 13:12:29 +0800

    card组件去除外层element组件

--- src/zys/components/zys_card.vue
+++ src/zys/components/zys_card.vue
@@ -1,23 +1,23 @@
 <template>
-  <el-col :span="5" style="margin-right:10px;width: 275px;">
-    <div class="home_card" :style="{height: card_height+'px'}">
-      <div>
-        <div class="home_card_header">
-          <span class="home_card_title">
-            <slot name="card_title"></slot>
-          </span>
+  <div class="home_card" :style="{
+    height: card_height+'px',
+    width: card_width+'px'}">
+    <div>
+      <div class="home_card_header">
+        <span class="home_card_title">
+          <slot name="card_title"></slot>
+        </span>
+      </div>
+      <div class="home_card_content">
+        <div class="home_card_link_item" v-for="link in card_links">
+          <router-link :to="{ name: link.name }">{{ link.title }}</router-link>
         </div>
-        <div class="home_card_content">
-          <div class="home_card_link_item" v-for="link in card_links">
-            <router-link :to="{ name: link.name }">{{ link.title }}</router-link>
-          </div>
-          <div class="home_card_link_item" v-for="link in card_outer_links">
-            <a :href="link.name" target="_blank">{{ link.title }}</a>
-          </div>
+        <div class="home_card_link_item" v-for="link in card_outer_links">
+          <a :href="link.name" target="_blank">{{ link.title }}</a>
         </div>
       </div>
     </div>
-  </el-col>
+  </div>
 </template>
 
 <script>
@@ -32,6 +32,11 @@ export default {
       required: false
     },
     card_height: {
+      type: Number,
+      required: false,
+      default: 200
+    },
+    card_width: {
       type: Number,
       required: false,
       default: 200
